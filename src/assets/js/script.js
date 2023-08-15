b9a3f9ac759185dbad7c40aa1eb38dcba2142810
whoafridi <afridirk10@gmail.com>
2023-08-16 00:52:55 +0600

    home, abput page updated with content

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -128,3 +128,45 @@ $(document).ready(function ($) {
       ],
     });
   });
+
+// packages 
+$(document).ready(function ($) {
+  // Initialize the Slick Carousel
+  $(".packages").slick({
+    centerMode: true,
+    centerPadding: '0px',
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,    
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});  
