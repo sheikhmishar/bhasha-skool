d9a6c361f71dc8f74dc0b87a47d9a276f40172fb
whoafridi <afridirk10@gmail.com>
2023-08-24 16:44:10 +0600

    navbar fix, about us slider added

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -225,3 +225,43 @@ $(document).ready(function ($) {
     ],
   });
 });  
+
+// stories
+$(document).ready(function ($) {
+  // Initialize the Slick Carousel
+  $(".stories").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
