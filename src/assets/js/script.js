623a83c7e49f53241a41b27a56dfac58e537711b
whoafridi <afridirk10@gmail.com>
2024-01-02 19:01:48 +0600

    slick slider issues fix when tab used

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -94,6 +94,44 @@ $(document).ready(function ($) {
       },
     ],
   });
+  $("#pills-tab button").click(function () {
+    $(".courses").slick("unslick"); // Destroy the existing Slick instance
+    $(".courses").slick({ 
+      dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    });
+  });
 });
 
 $(document).ready(function ($) {
